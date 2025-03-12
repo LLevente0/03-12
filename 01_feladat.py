6c6ffaee62ba71c857e8cc030e0faf5e4f9c8a9f
Ladányi Levente <ladanyi.levente@diak.szbi-pg.hu>
2025-03-12 13:12:18 +0100

    a

---
+++ 01_feladat.py
@@ -0,0 +1,7 @@
+"""1. Feladat
+A mellékelt fájl néhány ismert programozási nyelv adatát tartalmazza.
+Olvasd be a fájl tartalmát, és másold át azt egy fájlba úgy, hogy abba már csak a nyelv és az évszám kerüljön!
+(Fájl letöltése: kattints a "Forrásfájl" feliratú gombra az egér jobb gombjával,
+ és a felugró menüből válaszd a "Link mentése másként..." opciót!)"""
+
+
